8855be098da6311d817c8e40b347f4007b5e7feb
Tim Whiting <tim@whitings.org>
2023-04-06 11:12:35 -0600

    attempt

--- samples/basic/sort.cpp
+++ samples/basic/sort.cpp
@@ -4,8 +4,13 @@
 
 int main() {
   auto v = std::vector<int>{};
-  for (int i = 0; i < 1000000; ++i) {
+  for (int i = 0; i < 100000000; ++i) {
     v.push_back(random());
   }
   std::sort(v.begin(), v.end());
+  int sum = 0;
+  for (auto i : v) {
+    sum += i;
+  }
+  return sum;
 }
