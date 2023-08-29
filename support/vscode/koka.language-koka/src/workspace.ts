cc95f930e077df376ae0232a5a5572d68723c1ff
Tim Whiting <tim@whitings.org>
2023-08-29 09:17:01 -0600

    fix info message

--- support/vscode/koka.language-koka/src/workspace.ts
+++ support/vscode/koka.language-koka/src/workspace.ts
@@ -5,38 +5,38 @@ import * as os from "os"
 import * as vscode from "vscode"
 import * as child_process from "child_process"
 
-interface SDKs {sdkPath: string, allSDKs: string[]}
+interface SDKs { sdkPath: string, allSDKs: string[] }
 const kokaExeName = os.platform() === "win32" ? "koka.exe" : "koka"
 
 export function scanForSDK(): SDKs {
-  const processPath = (process.env.PATH as string ) || ""
+  const processPath = (process.env.PATH as string) || ""
   const paths = processPath.split(path.delimiter).filter((p) => p)
 
   const dev = path.join(process.env.HOME, 'koka')
   let defaultSDK = ""
   let allSDKs = []
-  if (fs.existsSync(dev)){
+  if (fs.existsSync(dev)) {
     const command = 'stack path --local-install-root'
-    const options = {cwd: dev, env: process.env}
+    const options = { cwd: dev, env: process.env }
     const result = child_process.execSync(command, options)
     const devPath = result.toString().trim();
     // Prioritize dev
-    if (fs.existsSync(devPath)){
+    if (fs.existsSync(devPath)) {
       vs.window.showInformationMessage("Koka dev SDK found!")
-      console.log("Koka: Found dev build of koka at " + devPath)
+      console.log("Koka: Using dev build of koka at " + devPath)
       defaultSDK = path.join(devPath, 'bin', kokaExeName)
       allSDKs.push(defaultSDK)
     }
   }
-  
+
   const local = path.join(process.env.HOME, '.local/bin')
-  for (const p of [local].concat(paths)){
-    if (fs.existsSync(path.join(p, kokaExeName))){
-      vs.window.showInformationMessage(`Using Koka SDK at ${p}`)
+  for (const p of [local].concat(paths)) {
+    if (fs.existsSync(path.join(p, kokaExeName))) {
       console.log("Koka: Found build of koka at " + p)
       const sdkPath = path.join(p, kokaExeName)
       allSDKs.push(sdkPath)
-      if (defaultSDK === ""){
+      if (defaultSDK === "") {
+        vs.window.showInformationMessage(`Using Koka SDK at ${p}`)
         defaultSDK = sdkPath
       }
     }
@@ -45,7 +45,7 @@ export function scanForSDK(): SDKs {
     console.log('Koka: No Koka SDK found')
     vs.window.showWarningMessage("Koka SDK not found on path or in ~/.local/bin")
   } else {
-    return {sdkPath: defaultSDK, allSDKs: allSDKs}
+    return { sdkPath: defaultSDK, allSDKs: allSDKs }
   }
 }
 
@@ -70,16 +70,16 @@ export class KokaConfig {
   target: string
   cwd: string
 
-  selectSDK(path: string){
-    if (!fs.existsSync(path)){
+  selectSDK(path: string) {
+    if (!fs.existsSync(path)) {
       return
     }
     this.command = this.config.get('languageServer.command') as string || `${this.sdkPath}`
     this.langServerArgs = ["--language-server", `-i${this.cwd}`]
   }
 
-  selectTarget(t: string){
-    if(!["C", "JS", "WASM", "C#"].includes(t)){
+  selectTarget(t: string) {
+    if (!["C", "JS", "WASM", "C#"].includes(t)) {
       return
     }
     this.target = t
