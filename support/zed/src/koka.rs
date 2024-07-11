6c59ce61be9a8acdcbb9a795d9731d4955a0df4a
Tim Whiting <tim@whitings.org>
2024-07-11 12:21:32 -0600

    work on zed support

--- support/zed/src/koka.rs
+++ support/zed/src/koka.rs
@@ -1,29 +1,67 @@
+use std::fs;
+use zed::settings::LspSettings;
 use zed::LanguageServerId;
 use zed_extension_api::{self as zed, Result};
 
-struct KokaExtension;
+struct KokaExtension {
+    cached_binary_path: Result<String>,
+}
+
+impl KokaExtension {
+    fn language_server_binary_path(
+        &mut self,
+        language_server_id: &LanguageServerId,
+        worktree: &zed::Worktree,
+    ) -> Result<String> {
+        if let Some(path) = LspSettings::for_worktree("koka", worktree)
+            .ok()
+            .and_then(|settings| {
+                settings.initialization_options.and_then(|options| {
+                    options
+                        .get("devPath")
+                        .and_then(|path| path.as_str().map(|p| p.to_string()))
+                })
+            })
+        {
+            if fs::metadata(path.clone()).map_or(false, |stat| stat.is_file()) {
+                return Ok(path);
+            }
+            return Err("Dev path invalid".to_string());
+        }
+
+        if let Ok(path) = &self.cached_binary_path {
+            if fs::metadata(path).map_or(false, |stat| stat.is_file()) {
+                return Ok(path.clone());
+            }
+        }
+
+        worktree.which("koka").ok_or("Koka not found".to_string())
+    }
+}
 
 impl zed::Extension for KokaExtension {
     fn new() -> Self {
-        Self
+        Self {
+            cached_binary_path: Err("No search performed yet".to_string()),
+        }
     }
 
     fn language_server_command(
         &mut self,
-        _language_server_id: &LanguageServerId,
+        language_server_id: &LanguageServerId,
         worktree: &zed::Worktree,
     ) -> Result<zed::Command> {
-        let Some(koka_bin) = worktree.which("koka") else {
-            return Err("koka not found".to_string());
-        };
-        Ok(zed::Command {
-            command: koka_bin.to_string(),
-            args: vec![
-                "--language-server".to_string(),
-                "--buildtag=zed".to_string(),
-                "--lsstdio".to_string(),
-            ],
-            env: Default::default(),
+        self.cached_binary_path = self.language_server_binary_path(language_server_id, worktree);
+        self.cached_binary_path.clone().and_then(|koka_bin| {
+            return Ok(zed::Command {
+                command: koka_bin.to_string(),
+                args: vec![
+                    "--language-server".to_string(),
+                    "--buildtag=zed".to_string(),
+                    "--lsstdio".to_string(),
+                ],
+                env: Default::default(),
+            });
         })
     }
 }
