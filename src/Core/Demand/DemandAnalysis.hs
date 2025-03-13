0620c00749aaa097ce18c7928a6bb003a360f45e
Tim Whiting <tim@whitings.org>
2025-03-13 15:52:20 -0600

    get rid of trace

--- src/Core/Demand/DemandAnalysis.hs
+++ src/Core/Demand/DemandAnalysis.hs
@@ -214,7 +214,7 @@ findUsage tname@TName{getName = name} ctx env = do
           visitEachChild ctx $ do
             childCtx <- currentContext <$> getEnv
             m <- contextLength <$> getEnv
-            trace ("m" ++ show m) $ return ()
+            -- trace ("m" ++ show m) $ return ()
             findUsage tname childCtx (limitmenv (EnvCtx (IndetCtx tn) env) m)
       _ -> childrenUsages
 
