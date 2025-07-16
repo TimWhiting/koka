16c11c8652cb1ae53ed298417e6fd3dbaa8f33ee
Tim Whiting <tim@whitings.org>
2025-07-16 09:47:02 -0600

    fix compiler errors after rebase

--- src/Type/InferMonad.hs
+++ src/Type/InferMonad.hs
@@ -103,7 +103,7 @@ import Common.Range hiding (Pos)
 import Common.Unique
 import Common.Failure
 import Common.Error(Error, ErrorKind(..), toWarning, ok, addWarnings, errorMessageKind, errorMsg)
-import Common.Syntax( Visibility(..))
+import Common.Syntax( Visibility(..), DefSort (..))
 import Common.File(endsWith,normalizeWith, seqqList)
 import Common.Name
 import Common.NamePrim(nameTpVoid,nameTpPure,nameTpIO,nameTpST,nameTpAsyncX,
@@ -1655,7 +1655,7 @@ resolveImplicitConstraints free ics
     resolve ic
       = do (evidence,tp) <- (icSolve ic) free ic
            solvedImplicitConstraint (icEvidence ic) tp
-           return $ Core.makeTDef (Core.TName (icEvidence ic) tp) evidence
+           return $ Core.makeTDef (Core.TName (icEvidence ic) tp Nothing) evidence
 
 
 tryResolveImplicitConstraints :: Bool -> Tvs -> Inf (Core.Expr -> Core.Expr)
@@ -1680,7 +1680,7 @@ tryResolveImplicitConstraints close free
            if determined || force || close
              then do (ev,tp) <- (icSolve ic) free ic
                      solvedImplicitConstraint (icEvidence ic) tp
-                     let def = Core.makeTDef (Core.TName (icEvidence ic) tp) ev
+                     let def = Core.makeTDef (Core.TName (icEvidence ic) tp Nothing) ev
                      tryResolve (def:defs, acc) ics
              else tryResolve (defs, ic:acc) ics
 
@@ -1787,7 +1787,7 @@ resolveHeapDivConstraint free ic
                   stp  <- subst tp
                   -- traceDefDoc $ \penv -> text "resolve @hdiv:" <+> Pretty.ppName penv (icEvidence ic) <.> colon <+> Pretty.ppType penv stp <+> text "as" <+> text (if maydiv then "divergent" else "non-divergent")
                   --                         <-> text "  , free: " <+> ppTvs penv free
-                  let ev = Core.TypeApp (coreExprFromNameInfo cname cinfo) [tpHeap,tpVal,seff]
+                  let ev = Core.TypeApp (coreExprFromNameInfo cname cinfo rangeNull) [tpHeap,tpVal,seff]
                   return (ev,stp)
 
 
