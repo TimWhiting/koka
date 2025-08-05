6f69565bdb3d77bf17eb2294d1b5fe3df46286f2
Tim Whiting <tim@whitings.org>
2025-08-05 15:34:28 -0600

    simplify

--- src/Core/FlowAnalysis/Full/KCFA/AbstractValue.hs
+++ src/Core/FlowAnalysis/Full/KCFA/AbstractValue.hs
@@ -52,12 +52,14 @@ type VEnv = M.Map TName StaticCtx
 
 data Addr =
   BindingAddr StaticCtx TName
+  | TopAddr TName
   | ImplicitAddr StaticCtx VEnv ExprContextId
   | ImplicitLAddr StaticCtx VEnv ExprContextId
   | BindImplicitAddr StaticCtx VEnv ExprContextId
   deriving (Eq, Ord)
 instance Show Addr where
   show (BindingAddr ctx name) = "B@(" ++ show name ++ ":" ++ show ctx ++ ")"
+  show (TopAddr name) = "T@(" ++ show name ++ ")"
   show (ImplicitAddr ctx env ctxId) = "AI@(" ++ showSimpleCtxId ctxId ++ ":" ++ show ctx ++ ")"
   show (ImplicitLAddr ctx env ctxId) = "IL@(" ++ showSimpleCtxId ctxId ++ ":" ++ show ctx ++ ")"
   show (BindImplicitAddr ctx env ctxId) = "BI@(" ++ showSimpleCtxId ctxId ++ ":" ++ show ctx ++ ")"
@@ -145,8 +147,6 @@ startEnv = M.empty
 endVAddr = BindingAddr startStaticCtx (TName (newName "endV") typeUnit Nothing)
 endKAddr = ImplicitAddr startStaticCtx startEnv (ExprContextId (-10001) (newName "endK"))
 endMKAddr = ImplicitAddr startStaticCtx startEnv (ExprContextId (-10002) (newName "endMK"))
-endTopMKAddr :: TName -> Addr
-endTopMKAddr name = ImplicitAddr startStaticCtx (M.singleton name startStaticCtx) (ExprContextId ((-1) * hash (nameStem (C.getName name))) (C.getName name))
 
 lookupEnv :: HasCallStack => TName -> VEnv -> Maybe Addr
 lookupEnv x env =
