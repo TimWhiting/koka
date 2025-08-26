719cd634263ccbdb304f502f4e7dec650751130e
Tim Whiting <tim@whitings.org>
2025-08-26 17:05:37 -0600

    updates

--- src/Compile/Build.hs
+++ src/Compile/Build.hs
@@ -376,6 +376,7 @@ moduleOptimize parsedMap tcheckedMap optimizedMap
                               sweepDM mT (d - 1) mT i
                             else do
                               sweepDM mT d (m - 1) i 
+                          -- trace ("Evaluating " ++ show m ++ show d) $ return ()
                           i flags{mSensitivity = m, dSensitivity = d}
                           
 
