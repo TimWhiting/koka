49cbfafc5156450e8fbb23bf20f1ffaf74b76d54
Tim Whiting <tim@whitings.org>
2025-08-29 16:57:42 -0600

    fix stuff

--- src/Compile/Build.hs
+++ src/Compile/Build.hs
@@ -363,12 +363,12 @@ moduleOptimize parsedMap tcheckedMap optimizedMap
                   let h = flagsHash flags
                       bc = seqString h $ BuildContext [modName mod] (mod:imports) h
                   when (analyze flags) $ do
-                    let bottomM = 1
-                    let bottomD = if rebinding flags then 1 else 0
+                    let bottomD = 0 -- if rebinding flags then 0 else 0
                     -- liftIO $ termInfo term (prettyCore defaultEnv (C CDefault) [] core)
                     let doSweep = sweep flags
                     let sweepDM :: Int -> Int -> Int -> (Flags -> Build ()) -> Build ()
                         sweepDM mT d m i = do
+                          let bottomM = if not (kcfa flags) then (if d == bottomD then 0 else 1) else 0
                           when doSweep $ do
                             if d == bottomD && m == bottomM then return ()
                             else if d == bottomD then do
--- src/Core/FlowAnalysis/Full/DMCFA/AbstractValue.hs
+++ src/Core/FlowAnalysis/Full/DMCFA/AbstractValue.hs
@@ -48,7 +48,13 @@ instance Show Call where
   show CallDelim = "delim"
   show (CallApp ctxId) = "a" ++ showSimpleCtxId ctxId
 
-type StaticCtx = [Call]
+data StaticCtx = 
+  TKTop [Call]
+  | TKDelim [Call]
+  deriving (Eq, Ord)
+instance Show StaticCtx where
+  show (TKTop calls) = "t_" ++ show calls
+  show (TKDelim calls) = "d_" ++ show calls
 
 type DynamicCtx = [(ExprContextId, StaticCtx)]
 
@@ -65,12 +71,13 @@ ctxHnd (CombinedCtx _ ((id, _): rst)) = id
 ctxHnd (CombinedCtx _ []) = ExprContextId (-5000) (newName "hnd")
 
 addCall :: Int -> CombinedCtx -> ExprContextId -> CombinedCtx
-addCall m (CombinedCtx static dyn) call = CombinedCtx (take m $ CallApp call : static) dyn
+addCall m (CombinedCtx (TKDelim static) dyn) call = CombinedCtx (TKDelim $ take m $ CallApp call : static) dyn
+addCall m (CombinedCtx (TKTop static) dyn) call = CombinedCtx (TKTop $ take m $ CallApp call : static) dyn
 
 addDelim :: Int -> CombinedCtx -> ExprContextId -> DynamicCtx
 addDelim d (CombinedCtx static dyn) delim = take d $ (delim, static) : dyn
 
-delimCtx m ctx = take m [CallDelim] -- take m ctx -- 
+delimCtx m ctx = TKDelim $ take m [CallDelim] -- take m ctx -- take m [CallDelim]
 
 newDelim d m (CombinedCtx static dyn) delim = CombinedCtx (delimCtx m static) $ take d $ (delim, static) : dyn
 
