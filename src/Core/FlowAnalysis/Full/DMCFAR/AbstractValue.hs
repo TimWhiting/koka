c788ff3662647df268a00ac12c3629e8e3a99c49
Tim Whiting <tim@whitings.org>
2025-08-29 23:07:36 -0600

    update

--- src/Core/FlowAnalysis/Full/DMCFAR/AbstractValue.hs
+++ src/Core/FlowAnalysis/Full/DMCFAR/AbstractValue.hs
@@ -137,7 +137,6 @@ data Frame =
       doCtx :: !ExprContextId,
       hCtx :: !ExprContextId,
       linkKnext :: !Addr,
-      llastCtx :: !CombinedCtx,
       linkHnd :: !Handler
   }
   | FDollar {
@@ -185,7 +184,7 @@ hvars (Handler _ _ v) = bvars v
 
 data MKont =
   MKEnd
-  | MKHandle { eff :: !Name, mkKNext:: !Addr, mknext:: !Addr, hnd :: !Handler, lastMkCtx :: !CombinedCtx, mkCtx:: !CombinedCtx }
+  | MKHandle { eff :: !Name, mkKNext:: !Addr, mknext:: !Addr, hnd :: !Handler, mkCtx:: !CombinedCtx }
   deriving (Eq, Ord, Show)
 
 startStaticCtx = [CallTop]
