64e845eb718afe78f1520c39fe640cc28c41bc34
Tim Whiting <tim@whitings.org>
2025-09-03 15:45:42 -0600

    fix kcfa

--- src/Core/FlowAnalysis/Full/DMCFA/DMCFA.hs
+++ src/Core/FlowAnalysis/Full/DMCFA/DMCFA.hs
@@ -149,6 +149,7 @@ doEval expr venv kaddr mkaddr ctx =
       apply kaddr mkaddr addr (dynamic ctx)
     Var name _ -> do
       if isPrimitive name then do
+        -- trace ("Primitive " ++ show name) $ return ()
         addr <- allocConst venv ctx expr (AChangePrim name expr)
         apply kaddr mkaddr addr (dynamic ctx)
       else if qualifier (getName name) == nameCoreHnd then
@@ -383,7 +384,8 @@ doUnwind name opName performExpr kaddr mkaddr args ctx = do
         AChangeObj _ tname hndargs@(_:ops) <- store hnd
         hargs <- mapM (store . snd) hndargs
         -- trace ("Unwinding: " ++ show (map fst ops) ++ " " ++ show opName ++ " " ++ show hargs) $ return ()
-        let unmakeHidden ('-':rest) = newName rest
+        let unmakeHidden ('@':'v':'a':'l':'-':op) = opName
+            unmakeHidden ('-':rest) = newName rest
             unmakeHidden (_:rest) = unmakeHidden rest
         let ops' = map (\(n, a) -> (unmakeHidden $ nameStem n, a)) ops
         case lookup opName ops' of
