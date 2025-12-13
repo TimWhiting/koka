2482e7c7a514c862a33c33f61f2e9f8c8b90eeb4
Tim Whiting <tim@whitings.org>
2025-12-12 22:46:10 -0700

    mutual recursion

--- src/Core/Divergent.hs
+++ src/Core/Divergent.hs
@@ -17,6 +17,7 @@ module Core.Divergent( analyzeDivergence ) where
 import Control.Applicative
 import Control.Monad
 import Data.List( transpose, permutations )
+import qualified Data.Map as M
 import Common.Name
 import Common.NamePrim( nameSubStr1, namesSameSize, nameEffectOpen, nameDecreasing )
 import Common.Failure
@@ -35,13 +36,32 @@ analyzeDivergence defs0
   = -- trace (" analyzeDivergence: " ++ show (map defName defs0)) $
     let mayDiverge = case defs0 of
                       [] -> False
-                      (def1:def2:rest) 
-                        -> True -- for mutual recursion, we assume it is divergent for now
                       [def0]
                         -> let (DefRec [def1]) = uniquefyDefGroup (DefRec [def0])
                            in isDivergent (def1)
-    in -- trace (" analyzeDivergence: " ++ show (map defName defs0) ++ ": " ++ show mayDiverge) $
+                      defs 
+                        -> let (DefRec defsUniq) = uniquefyDefGroup (DefRec defs)
+                               res = not (all (isTerminatingInGroup defsUniq) defsUniq)
+                           in res
+    in -- trace (" analyzeDivergence result: " ++ show (map defName defs0) ++ ": " ++ show mayDiverge) $
        mayDiverge
+
+isTerminatingInGroup :: [Def] -> Def -> Bool
+isTerminatingInGroup group def
+  = let siblings = filter (\d -> defName d /= defName def) group
+        def' = if null siblings 
+                 then def 
+                 else def { defExpr = pushLet [DefRec siblings] (defExpr def) }
+    in not (isDivergent def')
+
+pushLet :: [DefGroup] -> Expr -> Expr
+pushLet defs expr
+  = case expr of
+      TypeLam tpars e -> TypeLam tpars (pushLet defs e)
+      TypeApp e targs -> TypeApp (pushLet defs e) targs
+      Lam pars eff e  -> Lam pars eff (Let defs e)
+      Let defs' e     -> pushLet (defs ++ defs') e 
+      _               -> Let defs expr 
 
 -- | Is a recursive definition group potentially divergent? 
 -- Assumes that the the definitions are uniquefied.
@@ -82,8 +102,9 @@ isDivFun name pars body
 
 
 
-newtype Div a = Div (Rel -> (a,[Call]))
+newtype Div a = Div (Env -> (a,[Call]))
 
+data Env   = Env { envRel :: Rel, envDefs :: M.Map Name Def }
 data Rel   = Rel Name [S.NameSet] [S.NameSet]  -- set of equal and smaller variables per argument
 type Call  = [Size]       -- arguments sizes of a recursive call
 data Size  = Lt | Eq | Unknown
@@ -98,34 +119,43 @@ instance Show Size where
 runDiv :: Name -> [TName] -> Div a -> (a,[Call])
 runDiv defName args (Div d)
   = let rel = Rel defName (map (S.singleton . getName) args) (replicate (length args) S.empty)
-    in d rel
+        env = Env rel M.empty
+    in d env
 
 instance Functor Div where
-  fmap f (Div d)   = Div (\rel -> case d rel of (x,calls) -> (f x, calls))
+  fmap f (Div d)   = Div (\env -> case d env of (x,calls) -> (f x, calls))
 
 instance Applicative Div where
-  pure x  = Div (\rel -> (x,[]))
+  pure x  = Div (\env -> (x,[]))
   (<*>)   = ap  
 
 instance Monad Div where
   -- return = pure
-  (Div d) >>= f  = Div (\rel -> case d rel of
+  (Div d) >>= f  = Div (\env -> case d env of
                                   (x,calls1) -> case f x of
-                                                  Div d2 -> case d2 rel of
+                                                  Div d2 -> case d2 env of
                                                               (y,calls2) -> (y,calls1 ++ calls2))
       
 
 getRel :: Div Rel
 getRel
-  = Div (\rel -> (rel,[]))
+  = Div (\env -> (envRel env,[]))
 
 withRel :: Rel -> Div a -> Div a
 withRel rel (Div d)
-  = Div (\rel0 -> d rel)
+  = Div (\env -> d (env{ envRel = rel }))
+
+getDefs :: Div (M.Map Name Def)
+getDefs
+  = Div (\env -> (envDefs env, []))
+
+withDefs :: [Def] -> Div a -> Div a
+withDefs defs (Div d)
+  = Div (\env -> d (env{ envDefs = M.union (M.fromList [(defName d, d) | d <- defs]) (envDefs env) }))
 
 addCall :: Name -> [Size] -> Div ()
 addCall name call
-  = Div (\rel -> ((),[call]))
+  = Div (\env -> ((),[call]))
 
 
 at :: [S.NameSet] -> Int -> S.NameSet
@@ -168,6 +198,45 @@ addRelation sz name1 name2 div
                                       else (eq,lt)
              withRel (Rel rname eqs' lts') div     
 
+addParamRelation :: Int -> Size -> Name -> Div a -> Div a
+addParamRelation k sz name div
+  = if (sz == Unknown)
+     then div
+     else do (Rel rname eqs lts) <- getRel
+             let (eqs',lts') = unzip (zipWith3 add [0..] eqs lts)
+                 add i eq lt = if (i == k)
+                                then case sz of
+                                       Lt -> (eq,S.insert name lt)
+                                       Eq -> (S.insert name eq,lt)
+                                       _  -> failure "Core.Divergent.addParamRelation: encountered Unknown"
+                                else (eq,lt)
+             withRel (Rel rname eqs' lts') div
+
+bindParams :: [Name] -> [Expr] -> Div a -> Div a
+bindParams names args action = do
+  (Rel rname eqs lts) <- getRel
+  let numArgs = length eqs
+  updates <- forM (zip names args) $ \(paramName, argExpr) -> do
+               sizes <- forM [0..numArgs-1] $ \k -> do
+                          sz <- argumentSize rname (k, argExpr)
+                          return (k, sz)
+               return (paramName, sizes)
+  
+  let applyUpdates act = foldr (\(paramName, sizes) acc -> 
+                                  foldr (\(k, sz) acc2 -> addParamRelation k sz paramName acc2) acc sizes
+                               ) act updates
+  applyUpdates action
+
+inlineDef :: Def -> [Expr] -> Div ()
+inlineDef def args = do
+  let body = defExpr def
+  case body of
+    Lam pars _ expr -> do
+      -- trace ("inlining " ++ show (defName def)) $
+      bindParams (map getName pars) args (divExpr expr)
+    _ -> return ()
+
+
 
 
 divExpr :: Expr -> Div ()
@@ -188,7 +257,11 @@ divExpr expr
               if isRec 
                 then do call <- mapM (argumentSize (getName tname)) (zip [0..] args) -- todo: should we add 'Unknown's for partial applications?
                         addCall (getName tname) call
-                else return ()
+                else do
+                  defs <- getDefs
+                  case M.lookup (getName tname) defs of
+                    Just def -> inlineDef def args
+                    Nothing  -> return ()
               mapM_ divExpr args
 
       Var tname info  -- recursive call may appear as argument, say id(recfun)(x)
@@ -203,12 +276,23 @@ divExpr expr
       TypeApp expr tps 
         -> divExpr expr
       Let defGroups expr  
-        -> do mapM_ divDefGroup defGroups
-              divExpr expr
+        -> divDefGroups defGroups (divExpr expr)
       Case exprs branches 
         -> do mapM_ divExpr exprs
               mapM_ (divBranch exprs) branches
       _ -> return ()
+
+divDefGroups :: [DefGroup] -> Div () -> Div ()
+divDefGroups [] action = action
+divDefGroups (dg:dgs) action = do
+    case dg of
+       DefRec defs -> withDefs defs $ mapM_ divDef defs
+       DefNonRec def -> divDef def -- NonRec cannot call itself
+    
+    let defs = case dg of
+                 DefRec ds -> ds
+                 DefNonRec d -> [d]
+    withDefs defs (divDefGroups dgs action)
 
 divBranch :: [Expr] -> Branch -> Div ()
 divBranch exprs (Branch patterns guards)
@@ -239,13 +323,6 @@ divPattern size (mbName,pat)
       (_, _)
         -> return id
 
-
-divDefGroup :: DefGroup -> Div ()
-divDefGroup defgroup
-  = case defgroup of
-      DefNonRec def -> divDef def
-      DefRec defs   -> mapM_ divDef defs
-    
 divDef def
   = divExpr (defExpr def)
 
